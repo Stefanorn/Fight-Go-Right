daf8c433f96b7ce63dce796362f579e4f8488ac4
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2014-12-17 20:10:43 +0100

    Laðaðir littlir böggar og bætt inn plane og limited labbsvæði

---
+++ Assets/Scripts/LimitYMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimitYMovement : MonoBehaviour 
+{
+	public float minY = 0.0f;
+	public float maxY = 10.0f;
+
+
+	void LateUpdate () 
+	{
+		Vector3 newPosition = transform.position;
+		newPosition.y = Mathf.Clamp (newPosition.y, minY, maxY);
+		transform.position = newPosition;
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+
+		Vector3 origin = new Vector3 (-20.0f, minY, 0.0f);
+		Vector3 origin2 = new Vector3 (-20.0f, maxY, 0.0f);
+		Gizmos.DrawRay (origin, Vector3.right*100);
+		Gizmos.DrawRay (origin2, Vector3.right*100);
+
+	}
+}
