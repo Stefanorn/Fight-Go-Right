9cf1f6a487b524346fd724c0ebeb9c5a37a11846
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2014-12-17 21:06:01 +0100

    Bætt in Flying og Flying NegaPlayer

---
+++ Assets/Scripts/FlyOnTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyOnTrigger : MonoBehaviour {
+
+	private Animator animator;
+
+	void Start ()
+	{
+		animator = GetComponentInParent<Animator> ();
+	}
+
+	void OnTriggerEnter () 
+	{
+		animator.SetBool ("Fly", true);
+	}
+	void OnTriggerExit ()
+	{
+		animator.SetBool ("Fly", false);
+	}
+}
